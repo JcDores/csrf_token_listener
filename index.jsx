10fd5ecc0cef4c3c5bb021b5dc6f06791d7f3d1d
JcDores <j.dores@runtime-revolution.com>
2023-01-12 16:35:22 +0000

    Update index with AxiosCsrf & FetchCsrf Functions
    Update CsrfTokenListener Method with Mode (backwards compatability)

--- index.jsx
+++ index.jsx
@@ -1,3 +1,14 @@
-import CsrfTokenListener from "./listeners/FetchCsrfTokenListener";
+import FetchCsrfTokenListener from "./listeners/FetchCsrfTokenListener";
+import AxiosCsrfTokenListener from "./listeners/AxiosCsrfTokenListener";
 
-export { CsrfTokenListener }
+export const CsrfTokenListener = (backendEndpoint, sameOrigin = true, mode = 'fetch') => {
+  if (mode === 'fetch'){
+    FetchCsrfTokenListener(backendEndpoint, sameOrigin)
+  } else if (mode === 'axios') {
+    AxiosCsrfTokenListener(backendEndpoint, sameOrigin)
+  } else {
+    console.error('Mode not supported')
+  }
+}
+
+export { FetchCsrfTokenListener, AxiosCsrfTokenListener }
